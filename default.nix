acf3848b06c5a67e620cfda81ba390ece0c5d44e
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-12-08 18:09:36 -0300

    python36 -> python3

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> {}, ... }:
 with pkgs; stdenv.mkDerivation rec {
   name = "nix-cage";
-  buildInputs = [ python36 bubblewrap nix ];
+  buildInputs = [ python3 bubblewrap nix ];
   nativeBuildInputs = [ makeWrapper ];
   src = ./.;
 
